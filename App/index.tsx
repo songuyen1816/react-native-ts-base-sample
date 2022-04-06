788d8fe6b1806f9286693830e6b2cb89f7a79192
Son Nguyen <nguyenson1816@gmail.com>
2022-04-06 17:50:55 +0700

    update structure

--- App/index.tsx
+++ App/index.tsx
@@ -1,17 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Sample from './App/screens/Sample/Sample';
+import Sample from './screens/Sample/Sample';
 
 const Stack = createNativeStackNavigator();
 
