92edb877398f2bc8d9572033f4e4a5348da294b0
Son Nguyen <nguyenson1816@gmail.com>
2022-04-06 17:59:48 +0700

    Update structure

--- App/index.tsx
+++ App/index.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Sample from './screens/Sample/Sample';
+import Sample from './screens/Sample/SampleView';
 
 const Stack = createNativeStackNavigator();
 
